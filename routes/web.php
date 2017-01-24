f12247c7c9bf52e6b167c12f26623f90f6118715
David Martinez <davidmgilo@gmail.com>
2017-01-24 21:46:39 +0100

    sync

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,10 @@ Route::get('/sendemail', function () {
 });
 
 Route::get('/sendnotification', function () {
-    $user = factory(App\User::class)->create();
-    Auth::login($user);
- //   Auth::loginUsingId(1);
-//    $user= Auth::user();
+//    $user = factory(App\User::class)->create();
+//    Auth::login($user);
+    Auth::loginUsingId(1);
+    $user= Auth::user();
     $user->notify(new WelcomeNotification());
     dump("Notification sent!");
 });
